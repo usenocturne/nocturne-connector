a6d8f2ccecf9875f07d6a13763013f44c50382ab
Dominic Frye <me@itsnebula.net>
2025-02-20 17:54:59 -0500

    feat(01-wifi): set up wpa_supplicant control interface

--- input/stages/60/01-wifi.sh
+++ input/stages/60/01-wifi.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 chroot_exec apk add wireless-tools wpa_supplicant wpa_supplicant-openrc
+
+sed -i 's|default_conf=/etc/wpa_supplicant/wpa_supplicant.conf|default_conf=/data/etc/wpa_supplicant/wpa_supplicant.conf|' "$ROOTFS_PATH"/etc/init.d/wpa_supplicant
+mkdir -p "$DATAFS_PATH"/etc/wpa_supplicant
+cat > "$DATAFS_PATH"/etc/wpa_supplicant/wpa_supplicant.conf <<EOF
+ctrl_interface=/run/wpa_supplicant
+update_config=1
+EOF
+
 chroot_exec rc-update add wpa_supplicant default
-chroot_exec rc-update add wpa_cli boot
+chroot_exec rc-update add wpa_cli default
+
 echo "brcmfmac" >> "$ROOTFS_PATH"/etc/modules
 
 cat >> "$ROOTFS_PATH"/etc/network/interfaces.alpine-builder <<EOF
