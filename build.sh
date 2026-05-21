b0dd538ec3961e15da2f01b6b467df298db8663c
shadow <108803410+68p@users.noreply.github.com>
2026-05-21 11:12:10 -0400

    chore: bump to v2.0.1

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Image build config
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-: "${CONNECTOR_IMAGE_VERSION:="v2.0.0"}"
+: "${CONNECTOR_IMAGE_VERSION:="v2.0.1"}"
 
 : "${ALPINE_BUILD:="3.23"}"
 : "${ALPINE_BUILD_PATCH:="4"}"
