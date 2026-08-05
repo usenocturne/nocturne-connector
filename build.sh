1beea08d46403532040994e10bbf1eb4b59e3ebb
Dominic Frye <me@itsnebula.net>
2026-08-05 11:57:09 -0400

    chore: bump to v2.1.0

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Image build config
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-: "${CONNECTOR_IMAGE_VERSION:="v2.0.4-1"}"
+: "${CONNECTOR_IMAGE_VERSION:="v2.1.0"}"
 
 : "${UBOOT_PROJ_ID:="32838267"}"
 : "${UBOOT_TOOL_PROJ_ID:="33098050"}"
